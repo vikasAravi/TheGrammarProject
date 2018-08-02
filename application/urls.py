7b5db5aff7ed29285c684812cd595a8d93bf2789
vikasAravi <37362270+vikasAravi@users.noreply.github.com>
2018-08-02 10:23:10 +0530

    Update urls.py

--- application/urls.py
+++ application/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from application.views import index
 from application.views import custom,object_specific_view
 urlpatterns = [
-    path('createaccount/', index),
+    path('createaccount/', index , name = "signupform"),
     path('loginform/',custom, name = "loginform"),
     path('objects/<int:oid>/', object_specific_view, name = 'objects'),
     ]
